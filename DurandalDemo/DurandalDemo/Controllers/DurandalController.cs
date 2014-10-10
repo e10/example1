fda1c4b4a70fda5c1554b5cf8e5c1b9d6dfba21b
qanuj <a@e10.in>
2014-10-10 19:31:55 +0530

    Db Upgrade

--- DurandalDemo/DurandalDemo/Controllers/DurandalController.cs
+++ DurandalDemo/DurandalDemo/Controllers/DurandalController.cs
@@ -1,3 +1,7 @@
+using DurandalDemo.DAL;
+using DurandalDemo.Migrations;
+using System;
+using System.Data.Entity;
 using System.Web.Mvc;
 
 namespace DurandalDemo.Controllers {
@@ -5,5 +9,17 @@ namespace DurandalDemo.Controllers {
     public ActionResult Index() {
       return View();
     }
+    public ActionResult Upgrade() {
+        try {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ProspectContext, Configuration>());
+            using(var db = new ProspectContext()) {
+                db.Database.Initialize(true);
+                return Json(1, JsonRequestBehavior.AllowGet);
+            }
+        } catch(Exception ex) {
+            return Json(ex.Message, JsonRequestBehavior.AllowGet);
+        }
+    }
+
   }
 }
